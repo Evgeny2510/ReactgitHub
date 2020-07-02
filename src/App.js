ed13730168e577c9fe3296eaa269f732602e81d7
EvgenyChurilov <Evgeny2510@gmail.com>
2020-07-02 20:20:16 +0300

    git

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {HashRouter as  Router, Switch,Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+    
+<Router>
+<Link to='/'>HomePage</Link>
+<Link to='page2'>2</Link>
+
+<Switch>
+  <Route exact path='/' component={()=>{return <h1>HomePage works!!!</h1>}} />
+<Route exact Path='/page2' component={()=>{return <h1> page 2 works</h1>}} />
+
+</Switch>
+</Router>
+
+    </div>
+  );
+}
+
+export default App;
